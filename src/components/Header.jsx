7281eec4a9f1bcbc9d93bacda8ed98962697ec1c
YOrtiz <yortiz@blumonpay.com.mx>
2023-06-02 15:35:00 -0600

    Se añade y valida el  componente "Añadir presupuesto"

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import NuevoPresupuesto from "./NuevoPresupuesto"
+
+function Header({ presupuesto, setPresupuesto }) {
+    return (
+        <header>
+            <h1>Planificador de Gastos</h1>
+
+            <NuevoPresupuesto 
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+            />
+        </header>
+    )
+}
+
+export default Header
