807ab65f8acb82a613f9003556c262a243b83da1
YOrtiz <yortiz@blumonpay.com.mx>
2023-07-04 17:30:31 -0600

    Se añade reinicio de app y filtro de categorias.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
 import { useState, useEffect } from 'react';
 import Header from './components/Header';
+import Filtro from './components/Filtro';
 import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 import { generarId } from './helpers';
 import iconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
-  const [gastos, setGastos] = useState([]);
+  const [gastos, setGastos] = useState(
+    localStorage.getItem('gastos') ? JSON.parse(localStorage.getItem('gastos')) : []
+  );
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [presupuestoValido, setPresupuestoValido] = useState(false);
 
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false);
 
   const [gastoEditar, setGastoEditar] = useState({});
+
+  const [filtro, setFiltro] = useState('');
+  const [gastosFiltrados, setGastosFiltrados] = useState([]);
 
   useEffect(() => {
     if (Object.keys(gastoEditar).length > 0) {
@@ -24,7 +32,31 @@ function App() {
         setAnimarModal(true);
       }, 500);
     }
-  }, [gastoEditar])
+  }, [gastoEditar]);
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0);
+  }, [presupuesto]);
+
+  useEffect(() => {
+    const presupuestoLs = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLs > 0) {
+      setPresupuestoValido(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('gastos', JSON.stringify(gastos) ?? []);
+  }, [gastos]);
+
+  useEffect(() => {
+    if (filtro) {
+      const gastosFiltrados = gastos.filter(gasto => gasto.categoria === filtro);
+
+      setGastosFiltrados(gastosFiltrados);
+    }
+  }, [filtro]);
 
   const handelNuevoGasto = () => {
     setModal(true);
@@ -66,6 +98,7 @@ function App() {
       <div className={modal ? 'fijar' : ''}>
         <Header
           gastos={gastos}
+          setGastos={setGastos}
           presupuesto={presupuesto}
           setPresupuesto={setPresupuesto}
           presupuestoValido={presupuestoValido}
@@ -75,10 +108,16 @@ function App() {
         {presupuestoValido && (
           <>
             <main>
+              <Filtro
+                filtro={filtro}
+                setFiltro={setFiltro}
+              />
               <ListadoGastos
                 gastos={gastos}
                 setGastoEditar={setGastoEditar}
                 eliminarGasto={eliminarGasto}
+                filtro={filtro}
+                gastosFiltrados={gastosFiltrados}
               />
             </main>
             <div className='nuevo-gasto'>
